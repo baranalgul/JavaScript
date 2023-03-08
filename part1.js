eb240a301994239c1bef7329015690e9c4d9c9a3
baranalgul <brnalgul19@gmail.com>
2023-03-08 19:32:19 +0300

    I used console.log

---
+++ part1.js
@@ -0,0 +1,33 @@
+console.log("Hello")
+
+console.log("Hello", "Im learning JavaScript")
+
+// one line comment
+
+/*
+multi line comment
+*/
+
+// console.log('Hello Im Baran")  =>  syntax error
+
+
+// console.log("Hello Im Baran)   => syntax error
+// syntax error
+
+// console.log("Hello Im learning Javascript"  => syntax error
+
+// Mathematical Operations
+console.log(2 + 3) // 5 
+console.log(2 * 2) // 4
+console.log(10 - 4) // 6
+console.log(30 / 5) // 6
+console.log(3 ** 2) // 9
+console.log(30 % 6) // 0
+
+// Alert predefined js functions. Warning pop up appears
+alert("Javascript Lessons")
+// Prompt predefined js functions. Provides information with pop-up
+let name = prompt("Your name")
+document.write(name)
+// Confirm predefined js functions. Are you sure alert exc.
+confirm("Are you sure the blog post will be deleted?")
