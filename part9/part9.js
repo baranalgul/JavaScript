f28e32216e2a47815dc7b56f6dcadae201a632be
baranalgul <brnalgul19@gmail.com>
2023-03-29 22:12:56 +0300

    I used array and array methods

---
+++ part9/part9.js
@@ -0,0 +1,113 @@
+/* 
+
+ARRAYS
+
+Variables that can hold more than one value.
+It can contain more than one data type.
+They are usually defined with const.
+
+const array1 =['Param1', 'Param2', 'Param3']
+const array2 =Array('Param1', 'Param2', 'Param3')
+
+*/
+
+const id =['Kaan', 'Pamukcu', 'Programmer']
+
+console.log(id[2]) // Writes a Programmer to the screen.
+
+/* Nested arrays */
+
+const category= [
+    ['Computer', 'Console'],
+    ['Woman', 'Man', 'Children']
+]
+
+console.log(category[1][2])
+
+// split() -> Fragments of value. Splits the value and converts it to an array.
+
+let language = 'JavaScript'
+let split = language.split('')
+console.log(split)
+
+let img = 'img/facebook.png,img/instagram.png'
+let splitİmg = img.split(',')
+console.log(splitİmg)
+
+/* Arraylerin içeriğini değiştirme */
+
+const catList = ['House', 'Automobile', 'Electronic', 'Clothes']
+
+catList[0] = 'Teknology'
+console.log(catList)
+
+/* fill() -> Filling an empty array */
+
+const arr1 = Array(3).fill('Hello')
+console.log(arr1)
+
+/* concat() -> It is used to combine different strings. 
+               It can take more than one parameter.
+*/
+
+const womenClothes = ['skirt', 'blouse', 'body']
+const menClothes = ['trousers', 'shirt', 'shorts']
+
+const clothes1 = womenClothes.concat(menClothes)
+console.log(clothes1)
+
+const clothes2 = menClothes.concat(womenClothes)
+console.log(clothes2)
+
+const kidsClothes = ['bodysuit', 'hat', 'sock']
+const generalWear = womenClothes.concat(menClothes, kidsClothes)
+console.log(generalWear)
+
+/* 
+
+indexOf() -> Checks if a value (element) is in the array. Returns the index number, if any.
+
+*/
+
+const members1 = ['Burhan Altıntop', 'Hayko Cepkin', 'Mahmut Tuncer', 'Bülent Ersoy', 'Burhan Altıntop']
+
+console.log(members1.indexOf('Mahmut Tuncer'))
+
+let user = prompt('Enter the name')
+
+console.log(members1.indexOf(user))
+
+if(members1.indexOf(user) == -1 ) {
+    console.log('no member registration')
+} else {
+    console.log('member has registered')
+}
+const members2 = ['Burhan Altıntop', 'Hayko Cepkin', 'Mahmut Tuncer', 'Bülent Ersoy',]
+console.log(members2.indexOf('Burhan Altıntop')) // Writes 0 to the screen.
+console.log(members2.lastIndexOf('Burhan Altıntop')) // Weites 4 to the screen.
+
+/* 
+
+includes() -> It works just like indexOf() but returns either true or false as a result.
+
+*/
+
+console.log(members2.includes('Mahmut Tuncer')) // Writes true to the screen.
+
+let info = prompt('Enter the name')
+let check = members2.includes(info)
+
+if(check == true) {
+    console.log('member has registered')
+} else {
+    console.log('no member registration')
+}
+
+/* 
+
+toString() -> Converts the values of the array to string data type.
+
+*/
+
+const cat = ['Elektronic', 'Clothes', 'House']
+console.log(cat.toString()) // Writes Elektronic,Clothes,House to the screen
