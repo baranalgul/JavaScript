5bcb69f958bcd7c91bf6d91c72f1ddee45d96a3f
baranalgul <brnalgul19@gmail.com>
2023-06-12 18:37:21 +0300

    I made a selection app

---
+++ JavaScript Project/script.js
@@ -0,0 +1,51 @@
+// Şehirleri ve ilçeleri içeren bir veri yapısı
+var cities = {
+    turkey: ["Istanbul", "Ankara", "Izmir"],
+    usa: ["New York", "Los Angeles", "Chicago"]
+};
+
+var districts = {
+    istanbul: ["Kartal", "Maltepe", "Besiktas"],
+    ankara: ["Cankaya", "Sincan", "Kecioren"],
+    izmir: ["Konak", "Bornova", "Dikili"],
+    newyork: ["Manhattan", "Brooklyn", "Queens"],
+    losangeles: ["Hollywood", "Santa Monica", "Beverly Hills"],
+    chicago: ["Downtown", "Lincoln Park", "Wicker Park"]
+};
+
+
+function populateCities() {
+    var country = document.getElementById("country").value;
+    var citySelect = document.getElementById("city");
+    
+    citySelect.innerHTML = "<option value=''>Select City</option>";
+    
+    if (country !== "") {
+        var countryCities = cities[country];
+        
+        for (var i = 0; i < countryCities.length; i++) {
+            var option = document.createElement("option");
+            option.text = countryCities[i];
+            option.value = countryCities[i].toLowerCase();
+            citySelect.appendChild(option);
+        }
+    }
+}
+
+function populateDistricts() {
+    var city = document.getElementById("city").value;
+    var districtSelect = document.getElementById("district");
+
+    districtSelect.innerHTML = "<option value=''>Select District</option>";
+    
+    if (city !== "") {
+        var cityDistricts = districts[city];
+        
+        for (var i = 0; i < cityDistricts.length; i++) {
+            var option = document.createElement("option");
+            option.text = cityDistricts[i];
+            option.value = cityDistricts[i].toLowerCase().replace(/\s+/g, '');
+            districtSelect.appendChild(option);
+        }
+    }
+}
